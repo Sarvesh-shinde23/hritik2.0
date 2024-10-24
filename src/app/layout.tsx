1e4254299f660ccac38fe63f3288460c6d54f65f
Sarvesh-shinde23 <shindesarvesh023@gmail.com>
2024-10-24 20:37:37 +0530

    last commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
 
-    <html lang="en">
+    <html lang="en" data-theme="winter">
       <body className={montserrat.className}>
         
         <Navbar />
